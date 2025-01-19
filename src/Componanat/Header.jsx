188b1cba090c21e7ef98d57ca1f20327a53fd4f4
Ghulam Mustafa Memon <ghulammemon875@gmail.com>
2025-01-19 16:35:31 +0500

    add a upate resume link

--- src/Componanat/Header.jsx
+++ src/Componanat/Header.jsx
@@ -65,7 +65,7 @@ const Header = () => {
 
         <div className="hidden md:block">
           <Link
-            to="https://drive.google.com/file/d/1GeXFJs4mOsfQ0qCjO4_YKBT4ORwyeUCI/view?usp=drive_link"
+            to="https://drive.google.com/file/d/1tgABUYTddBmTA1HgCWnddmsxGRbeAf-M/view?usp=drive_link"
             target="blank"
             className="px-4 py-2 bg-orange-500 text-sm shadow rounded-full text-white"
           >
